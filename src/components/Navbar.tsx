aa35a33eb449109ff01414b5981f578389d9bd60
mugenosantyago <147428965+mugenosantyago@users.noreply.github.com>
2025-05-25 18:47:02 -0500

    yargo 夢幻のサンチャゴ

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,20 +3,19 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <header className="navbar sticky-top flex-md-nowrap">
-      {/* Original structure had this div, but it seems empty/commented out in parts?
-          Adjust if needed based on original full design intent.
-      <div className="col-md-3 col-lg-3 me-0 px-3 fs-6"> 
+      {/* Using flexbox utilities to space out content */}
+      <div className="container-fluid d-flex align-items-center">
+        {/* Keeping the brand link, adjust class if needed for spacing/styling */}
+        <Link className="navbar-brand me-auto px-3 fs-6" href="/"> {/* Use me-auto to push to the right */}
+           サンチャゴ
+        </Link>
+        {/* Add Toggler button if sidebar needs to be collapsible on mobile */}
+        {/* 
+        <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button> 
+        */}
       </div>
-      */}
-      <Link className="navbar-brand col-md-3 col-lg-3 me-0 px-3 fs-6" href="/">
-         サンチャゴ
-      </Link>
-      {/* Add Toggler button if sidebar needs to be collapsible on mobile */}
-      {/* 
-      <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button> 
-      */}
     </header>
   );
 } 
